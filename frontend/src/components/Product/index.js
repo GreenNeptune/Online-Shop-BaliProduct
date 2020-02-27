07fe375c3bde4e454b7adb4bf6e2aaed2e6cbbc6
GreenNeptune <silentwisdom75@gmail.com>
2020-02-27 12:12:24 +0200

    modify Product component

--- frontend/src/components/Product/index.js
+++ frontend/src/components/Product/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-function Product({title,price,quantity}) {
+function Product({ title, price, quantity, img }) {
   return (
     <div className="product_description">
-      <p className="title">{title}</p>
-      <p className="price">{price}</p>
-      <p className="quantity">{quantity}</p>$
+      <div className="product_img">
+        <img src={img} alt="" />
+      </div>
+      <p className="product_title">{title}</p>
+      <p className="product_price">{price}</p>
+      <p className="product_quantity">{quantity}</p>$
     </div>
   );
 }
 
 Product.prototype = {
+  img: PropTypes.string,
   title: PropTypes.string,
   price: PropTypes.number,
   quantity: PropTypes.number,
