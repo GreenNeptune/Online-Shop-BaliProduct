12d2bee936cf60693c5fe368da1d2688e8ecd397
GreenNeptune <silentwisdom75@gmail.com>
2020-06-11 16:25:32 +0300

    add Nav &  install nodemon

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Nav from './components/Nav';
 import './styles/App.scss';
 
 // Redux 
@@ -6,10 +7,9 @@ import store from '../src/redux_store'
 import { Provider } from 'react-redux';
 import CartContainer from './components/containers/CartContainer';
 import ProductsContainer from './components/containers/ProductsContainer';
-import AutoComplete from './components/AutoComplete';
 
 // router 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PrivateRoute from './components/routing/PrivateRoute';
 
 function App() {
@@ -17,19 +17,7 @@ function App() {
     <div className="App">
       <Provider store={store}>
         <Router>
-          <div className="nav">
-            <ul className="nav-links-left">
-              <Link to='/'>
-                <li style={{ boxShadow: '0 2rem 8rem rgba(0, 0, 0, 0.15)' }}>BaliProduct</li>
-              </Link>
-              <AutoComplete />
-              <Link to='/cart'>
-                <li className="logo">
-                  <i className="fas fa-shopping-cart fa-3x"></i>
-                </li>
-              </Link>
-            </ul>
-          </div>
+          <Nav />
           <Switch >
             < Route exact path='/' component={ProductsContainer} />
             < PrivateRoute exact path='/cart' component={CartContainer} />
