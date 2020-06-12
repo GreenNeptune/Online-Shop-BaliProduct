2e888fbdde00f31dd7842ac12a20f109fb64c0c6
GreenNeptune <silentwisdom75@gmail.com>
2020-06-12 17:03:32 +0300

    add Spinner when loading products

--- client/src/components/containers/ProductsContainer/index.js
+++ client/src/components/containers/ProductsContainer/index.js
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
-
 import { connect } from "react-redux";
 import { fetchProducts } from '../../../redux_store/reducers/products/actions';
 import { addProductToCart } from "../../../redux_store/reducers/cart/actions";
 import ProductList from '../../ProductList'
+import Spinner from '../../Spinner';
 
 function ProductsContainer({ products: { visibleProducts, loading }, fetchProducts, addProductToCart }) {
   useEffect(() => {
     fetchProducts();
   }, [fetchProducts]);
 
-  return (loading ? 'loading...' : (
+  return (loading ? (<Spinner />) : (
     <div className="products container">
       < ProductList products={visibleProducts} addProduct={addProductToCart} />
     </div>
