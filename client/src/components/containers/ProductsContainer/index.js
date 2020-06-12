ab8084aa68cb3bde6448bdf29a62eaeb8332833a
GreenNeptune <silentwisdom75@gmail.com>
2020-06-12 13:03:29 +0300

    insert fetchProducts prop inside useeffect dependency array

--- client/src/components/containers/ProductsContainer/index.js
+++ client/src/components/containers/ProductsContainer/index.js
@@ -3,13 +3,12 @@ import React, { useEffect } from 'react';
 import { connect } from "react-redux";
 import { fetchProducts } from '../../../redux_store/reducers/products/actions';
 import { addProductToCart } from "../../../redux_store/reducers/cart/actions";
-
 import ProductList from '../../ProductList'
 
 function ProductsContainer({ products, fetchProducts, addProductToCart }) {
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [fetchProducts]);
 
   return (
     <div className="products container">
