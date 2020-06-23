0ed44718aac6bbfad75ec0c905fbb6a99e506638
GreenNeptune <silentwisdom75@gmail.com>
2020-06-23 23:27:05 +0300

    fix getProductsTotalPrice

--- client/src/utils/getProductsTotalPrice.js
+++ client/src/utils/getProductsTotalPrice.js
@@ -1,5 +1,5 @@
 const getProductsTotalPrice = (products) => {
-  return products.reduce((total, product) => total + product.price, 0);
+  return products.reduce((total, product) => total + (product.price * product.quantity), 0);
 }
 export default getProductsTotalPrice;
 
