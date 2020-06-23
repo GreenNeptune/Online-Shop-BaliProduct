cda74b1ab588db85b5e63dd54ec9d56ef139254d
GreenNeptune <silentwisdom75@gmail.com>
2020-06-23 18:52:47 +0300

    modify addProductToCart

--- client/src/redux_store/reducers/cart/actions.js
+++ client/src/redux_store/reducers/cart/actions.js
@@ -1,9 +1,8 @@
 import { ADD_PRODUCT_TO_CART } from "./actionTypes"
 
-
-export const addProductToCart = product => {
-  return {
+export const addProductToCart = product => dispatch => {
+  dispatch({
     type: ADD_PRODUCT_TO_CART,
     payload: product
-  }
-} 
+  });
+}
