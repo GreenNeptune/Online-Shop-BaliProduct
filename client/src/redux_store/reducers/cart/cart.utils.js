a15f92934d8068ee8a60b1fc0f6a9886d9d7a669
GreenNeptune <silentwisdom75@gmail.com>
2020-06-23 18:56:34 +0300

    modify cart.utils.js & remove total from cart reducer

--- client/src/redux_store/reducers/cart/cart.utils.js
+++ client/src/redux_store/reducers/cart/cart.utils.js
@@ -1,10 +1,11 @@
 export const addProductToCart = (products, newProduct) => {
-  const isNewProductExist = products.find(product => product.id === newProduct.id);
+  const isNewProductExist = products.find(product => product._id === newProduct._id);
   if (isNewProductExist) {
-    return products.map(product => product.id === newProduct.id ? { ...product, count: product.count + 1 } : product);
+    return [...products];
   }
-  return [...products, { ...newProduct, count: 1 }];
+  return [...products, { ...newProduct, quantity: 1 }];
 
 }
 
-export const getCartTotal = (products) => products.reduce((total, product) => total + product.price * product.count, 0);
+
+export const getCartTotal = (products) => products.reduce((total, product) => total + product.price * product.quantity, 0);
--- client/src/redux_store/reducers/cart/index.js
+++ client/src/redux_store/reducers/cart/index.js
@@ -1,20 +1,30 @@
-import { ADD_PRODUCT_TO_CART } from "./actionTypes";
-import { addProductToCart, getCartTotal } from "./cart.utils";
+import { ADD_PRODUCT_TO_CART, REMOVE_PRODUCT_FROM_CART, UPDATE_PRODUCT_QUANTITY_IN_CART } from "./actionTypes";
+import { addProductToCart } from "./cart.utils";
 
 
 const initialState = {
   products: [],
-  total: 0
 }
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case ADD_PRODUCT_TO_CART:
-      const products = addProductToCart([...state.products], action.payload)
       return {
         ...state,
-        products: products,
-        total: getCartTotal(products)
+        products: addProductToCart([...state.products], action.payload),
+      }
+    case REMOVE_PRODUCT_FROM_CART:
+      return {
+        ...state,
+        products: [...state.products].filter(product => product._id !== action.payload)
+      }
+    case UPDATE_PRODUCT_QUANTITY_IN_CART:
+      return {
+        ...state,
+        products: [...state.products].map(product => product._id === action.payload.id
+          ? { ...product, quantity: action.payload.newQuantity }
+          : product
+        )
       }
     default:
       return state
