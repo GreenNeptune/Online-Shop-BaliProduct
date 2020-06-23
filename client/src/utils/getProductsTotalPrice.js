72a21fb7cc627b42d72e90a8735ec81a96eb6b44
GreenNeptune <silentwisdom75@gmail.com>
2020-06-24 00:14:37 +0300

    delete getProductsTotalPrice.js

--- client/src/utils/getProductsTotalPrice.js
+++
@@ -1,5 +0,0 @@
-const getProductsTotalPrice = (products) => {
-  return products.reduce((total, product) => total + (product.price * product.quantity), 0);
-}
-export default getProductsTotalPrice;
-
